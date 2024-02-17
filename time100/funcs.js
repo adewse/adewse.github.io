a8a9640b2469cdadf6e62335352a9f3d3323514d
adewse <85646602+adewse@users.noreply.github.com>
2024-02-17 21:23:54 +0300

    edit

--- time100/funcs.js
+++ time100/funcs.js
@@ -1,8 +1,9 @@
 function addTime(){
   vrem = document.createElement('a');
   vrem.className = 'time';
-  vrem.setAttribute('data-tz', 'Europe/Moscow');
-  vrem.setAttribute('data-format', '%s');
+  vrem.setAttribute('data-tz', 'Asia/Bangkok');
+  vrem.setAttribute('data-format', '%H:%i:%s');
+  vrem.style = 'position:fixed;bottom:0;left:20px;z-index:100000'
   vrem.innerHTML = 'время';
   document.body.append(vrem);
   
